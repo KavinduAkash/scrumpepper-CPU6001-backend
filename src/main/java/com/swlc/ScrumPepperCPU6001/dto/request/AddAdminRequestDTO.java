bb85478c50a6d440a040ebe010e4d66bdad002bf
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-13 23:13:20 +0530

    create AddAdminRequestDTO class

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/dto/request/AddAdminRequestDTO.java
@@ -0,0 +1,45 @@
+package com.swlc.ScrumPepperCPU6001.dto.request;
+
+import com.swlc.ScrumPepperCPU6001.enums.AdminType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * @author hp
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddAdminRequestDTO {
+    private long id;
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+    private String contactNumber;
+    private Date createdDate;
+    private AdminType adminType;
+
+    @Override
+    public String toString() {
+        return "AddAdminRequestDTO{" +
+                "id=" + id +
+                ", employeeId='" + employeeId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", contactNumber='" + contactNumber + '\'' +
+                ", createdDate=" + createdDate +
+                ", adminType=" + adminType +
+                '}';
+    }
+}
