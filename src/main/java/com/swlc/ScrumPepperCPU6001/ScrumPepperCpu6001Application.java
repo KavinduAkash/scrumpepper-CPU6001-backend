19eb4f2947c201d7a782b08c81fcc8d1fdcf7779
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-13 19:38:48 +0530

    initial commit

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/ScrumPepperCpu6001Application.java
@@ -0,0 +1,13 @@
+package com.swlc.ScrumPepperCPU6001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumPepperCpu6001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumPepperCpu6001Application.class, args);
+	}
+
+}
