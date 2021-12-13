899359ecd5dc8b9e2fe7bc77d6ebebca55cdb29e
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-13 21:29:11 +0530

    create application constant class

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/constant/ApplicationConstant.java
@@ -0,0 +1,14 @@
+package com.swlc.ScrumPepperCPU6001.constant;
+
+/**
+ * @author hp
+ */
+public class ApplicationConstant {
+
+    /**
+     * Exception error message constants
+     */
+    //don't use this variable for other error messages (only exception class use)
+    public static final String APPLICATION_ERROR_OCCURRED_MESSAGE = "Application Error Occurred";
+
+}
