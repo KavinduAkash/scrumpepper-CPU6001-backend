2db25c19994d64e1aaff5f564a27772f8e444773
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-13 23:01:52 +0530

    add new constant for resource already exist

--- src/main/java/com/swlc/ScrumPepperCPU6001/constant/ApplicationConstant.java
+++ src/main/java/com/swlc/ScrumPepperCPU6001/constant/ApplicationConstant.java
@@ -12,5 +12,6 @@ public class ApplicationConstant {
     public static final String APPLICATION_ERROR_OCCURRED_MESSAGE = "Application Error Occurred";
 
     public static final int RESOURCE_NOT_FOUND = 404;
+    public static final int RESOURCE_ALREADY_EXIST = 409;
 
 }
