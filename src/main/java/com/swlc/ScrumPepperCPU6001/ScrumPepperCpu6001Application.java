a01f87dab7fe4d032b884c557a0ae69577f5d149
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-13 20:41:28 +0530

    extend ScrumPepperCpu6001Application class by SpringBootServletInitializer

--- src/main/java/com/swlc/ScrumPepperCPU6001/ScrumPepperCpu6001Application.java
+++ src/main/java/com/swlc/ScrumPepperCPU6001/ScrumPepperCpu6001Application.java
@@ -2,9 +2,16 @@ package com.swlc.ScrumPepperCPU6001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScrumPepperCpu6001Application {
+public class ScrumPepperCpu6001Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ScrumPepperCpu6001Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrumPepperCpu6001Application.class, args);
