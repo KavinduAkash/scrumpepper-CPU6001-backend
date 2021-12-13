5f03ffe81d6a676bc34ce937d813d45910186f3b
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-13 21:32:08 +0530

    modify application constants

--- src/main/java/com/swlc/ScrumPepperCPU6001/constant/ApplicationConstant.java
+++ src/main/java/com/swlc/ScrumPepperCPU6001/constant/ApplicationConstant.java
@@ -11,4 +11,6 @@ public class ApplicationConstant {
     //don't use this variable for other error messages (only exception class use)
     public static final String APPLICATION_ERROR_OCCURRED_MESSAGE = "Application Error Occurred";
 
+    public static final int RESOURCE_NOT_FOUND = 404;
+
 }
