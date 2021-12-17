82fa5bfe0b81008dbf9d9d91bd851978ab2f5ef8
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-17 23:50:40 +0530

    create user repository

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.swlc.ScrumPepperCPU6001.repository;
+
+import com.swlc.ScrumPepperCPU6001.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * @author hp
+ */
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
