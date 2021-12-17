bf82da7eea51b53c6395f64152a5d97ceafb995f
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-17 22:44:40 +0530

    create native query to filter admin by status type

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/constant/NativeQueryConstant.java
@@ -0,0 +1,8 @@
+package com.swlc.ScrumPepperCPU6001.constant;
+
+/**
+ * @author hp
+ */
+public class NativeQueryConstant {
+    public static final String FILTER_ADMIN_BY_STATUS = "SELECT * FROM admin a WHERE a.status_type=?1";
+}
