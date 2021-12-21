d5508d7b1986969010d0cd7697b542ce41e477b6
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-21 21:28:36 +0530

    create AddCorporateEmployeeRequestDTO

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/dto/request/AddCorporateEmployeeRequestDTO.java
@@ -0,0 +1,26 @@
+package com.swlc.ScrumPepperCPU6001.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author hp
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddCorporateEmployeeRequestDTO {
+    private long userId;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "AddCorporateEmployeeRequestDTO{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
