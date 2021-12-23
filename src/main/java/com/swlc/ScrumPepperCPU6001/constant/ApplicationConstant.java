2b753303bcae2ab8fc99602563f0195c9fe7694c
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-23 11:17:59 +0530

    modify application constants

--- src/main/java/com/swlc/ScrumPepperCPU6001/constant/ApplicationConstant.java
+++ src/main/java/com/swlc/ScrumPepperCPU6001/constant/ApplicationConstant.java
@@ -15,5 +15,6 @@ public class ApplicationConstant {
     public static final int RESOURCE_NOT_FOUND = 404;
     public static final int RESOURCE_ALREADY_EXIST = 409;
     public static final int UN_AUTH_ACTION = 491;
+    public static final int UNABLE_TO_SEND_EMAIL = 422;
 
 }
