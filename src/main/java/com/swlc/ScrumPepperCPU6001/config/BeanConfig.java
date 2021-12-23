4052c95cba1ba14b5281b72b41e1cfba16c2a02c
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-23 20:22:32 +0530

    add restTemplate bean

--- src/main/java/com/swlc/ScrumPepperCPU6001/config/BeanConfig.java
+++ src/main/java/com/swlc/ScrumPepperCPU6001/config/BeanConfig.java
@@ -2,9 +2,11 @@ package com.swlc.ScrumPepperCPU6001.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author hp
@@ -15,5 +17,13 @@ public class BeanConfig {
     @Bean
     public JavaMailSender javaMailSender() {
         return new JavaMailSenderImpl();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setReadTimeout(600000);
+        requestFactory.setConnectTimeout(600000);
+        return new RestTemplate(requestFactory);
     }
 }
