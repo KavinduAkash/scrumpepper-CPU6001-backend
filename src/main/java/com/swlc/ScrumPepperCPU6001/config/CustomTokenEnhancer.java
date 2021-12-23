17bdfd051be258de50473d6d76873975997f0e00
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-23 21:03:48 +0530

    modify CustomTokenEnhancer to customize user token

--- src/main/java/com/swlc/ScrumPepperCPU6001/config/CustomTokenEnhancer.java
+++ src/main/java/com/swlc/ScrumPepperCPU6001/config/CustomTokenEnhancer.java
@@ -1,7 +1,10 @@
 package com.swlc.ScrumPepperCPU6001.config;
 
+import com.swlc.ScrumPepperCPU6001.constant.OAuth2Constant;
 import com.swlc.ScrumPepperCPU6001.dto.AdminDTO;
+import com.swlc.ScrumPepperCPU6001.dto.UserDTO;
 import com.swlc.ScrumPepperCPU6001.service.AdminService;
+import com.swlc.ScrumPepperCPU6001.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,10 +25,12 @@ import java.util.Map;
 public class CustomTokenEnhancer extends JwtAccessTokenConverter {
 
     private final AdminService adminService;
+    private final UserService userService;
 
     @Autowired
-    public CustomTokenEnhancer(AdminService adminService) {
+    public CustomTokenEnhancer(AdminService adminService, UserService userService) {
         this.adminService = adminService;
+        this.userService = userService;
     }
 
     @Override
@@ -37,9 +42,15 @@ public class CustomTokenEnhancer extends JwtAccessTokenConverter {
         UsernamePasswordAuthenticationToken authentication =
                 (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
 
-        AdminDTO adminDetailsByUserName = adminService.getAdminDetailsByUserName(user.getUsername());
-        additionalInfo.put("user", adminDetailsByUserName);
-        additionalInfo.put("user_id", adminDetailsByUserName.getId());
+        if (user.getUsername().equals(OAuth2Constant.ADMIN_CLIENT_ID)) {
+            AdminDTO adminDetailsByUserName = adminService.getAdminDetailsByUserName(user.getUsername());
+            additionalInfo.put("user", adminDetailsByUserName);
+            additionalInfo.put("user_id", adminDetailsByUserName.getId());
+        } else {
+            UserDTO userDetailsByEmail = userService.getUserDetailsByEmail(user.getUsername());
+            additionalInfo.put("user", userDetailsByEmail);
+            additionalInfo.put("user_id", userDetailsByEmail.getId());
+        }
 
         ((DefaultOAuth2AccessToken) oAuth2AccessToken).setAdditionalInformation(additionalInfo);
         // set custom claims
