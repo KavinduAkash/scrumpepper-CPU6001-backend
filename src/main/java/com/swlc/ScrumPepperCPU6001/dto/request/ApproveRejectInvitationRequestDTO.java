42f0220c1392e1edf4f7a6ac4507c2dd8a2b6760
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-25 10:36:59 +0530

    create ApproveRejectInvitationRequestDTO

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/dto/request/ApproveRejectInvitationRequestDTO.java
@@ -0,0 +1,9 @@
+package com.swlc.ScrumPepperCPU6001.dto.request;
+
+/**
+ * @author hp
+ */
+public class ApproveRejectInvitationRequestDTO {
+    private long invitationId;
+    private String invitationStatus;
+}
