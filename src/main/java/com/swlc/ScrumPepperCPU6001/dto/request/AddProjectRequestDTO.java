fc298ef472c1f65a78e87efd6459d9b71a162196
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-26 19:15:27 +0530

    add getter, setter, arg and no-arg constructor annotations to AddProjectRequestDTO

--- src/main/java/com/swlc/ScrumPepperCPU6001/dto/request/AddProjectRequestDTO.java
+++ src/main/java/com/swlc/ScrumPepperCPU6001/dto/request/AddProjectRequestDTO.java
@@ -1,8 +1,17 @@
 package com.swlc.ScrumPepperCPU6001.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author hp
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddProjectRequestDTO {
     private long corporateId;
     private String projectName;
