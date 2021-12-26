f0f85b57b6d2d157cde668da53bdc30fecaf27ad
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-26 22:03:16 +0530

    create ProjectMemberRepository

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/repository/ProjectMemberRepository.java
@@ -0,0 +1,10 @@
+package com.swlc.ScrumPepperCPU6001.repository;
+
+import com.swlc.ScrumPepperCPU6001.entity.ProjectMemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author hp
+ */
+public interface ProjectMemberRepository extends JpaRepository<ProjectMemberEntity, Long> {
+}
