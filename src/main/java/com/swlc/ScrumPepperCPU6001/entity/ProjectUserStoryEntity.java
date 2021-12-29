de4d1d18bc021c52504af9937c2c43ec4f6743ff
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-12-29 09:01:22 +0530

    create ProjectUserStoryEntity

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/entity/ProjectUserStoryEntity.java
@@ -0,0 +1,73 @@
+package com.swlc.ScrumPepperCPU6001.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.swlc.ScrumPepperCPU6001.enums.UserStoryStatusType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author hp
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "project_user_story")
+public class ProjectUserStoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="project_id")
+    private ProjectEntity projectEntity;
+    @Column(nullable = false)
+    private String title;
+    @Column(length = 10000)
+    private String description;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date modifiedDate;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="created_by")
+    private CorporateEmployeeEntity createdBy;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="modified_by")
+    private CorporateEmployeeEntity modifiedBy;
+    @Enumerated(EnumType.STRING)
+    private UserStoryStatusType statusType;
+
+    public ProjectUserStoryEntity(ProjectEntity projectEntity, String title, String description, Date createdDate,
+                                  Date modifiedDate, CorporateEmployeeEntity createdBy,
+                                  CorporateEmployeeEntity modifiedBy, UserStoryStatusType statusType) {
+        this.projectEntity = projectEntity;
+        this.title = title;
+        this.description = description;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+        this.createdBy = createdBy;
+        this.modifiedBy = modifiedBy;
+        this.statusType = statusType;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectUserStoryEntity{" +
+                "id=" + id +
+                ", projectEntity=" + projectEntity +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", createdDate=" + createdDate +
+                ", modifiedDate=" + modifiedDate +
+                ", createdBy=" + createdBy +
+                ", modifiedBy=" + modifiedBy +
+                ", statusType=" + statusType +
+                '}';
+    }
+}
