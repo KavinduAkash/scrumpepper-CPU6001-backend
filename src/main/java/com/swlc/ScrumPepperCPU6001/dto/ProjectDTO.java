dbb816d0ac72c4ed700a795fd1902fe83f671b90
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-01-07 08:38:12 +0530

    create ProjectDTO

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/dto/ProjectDTO.java
@@ -0,0 +1,41 @@
+package com.swlc.ScrumPepperCPU6001.dto;
+
+import com.swlc.ScrumPepperCPU6001.enums.ProjectStatusType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * @author hp
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectDTO {
+    private long id;
+    private CorporateDTO corporate;
+    private String projectName;
+    private Date createdDate;
+    private Date modifiedDate;
+    private CorporateEmployeeDTO createdBy;
+    private CorporateEmployeeDTO modifiedBy;
+    private ProjectStatusType statusType;
+
+    @Override
+    public String toString() {
+        return "ProjectDTO{" +
+                "id=" + id +
+                ", corporate=" + corporate +
+                ", projectName='" + projectName + '\'' +
+                ", createdDate=" + createdDate +
+                ", modifiedDate=" + modifiedDate +
+                ", createdBy=" + createdBy +
+                ", modifiedBy=" + modifiedBy +
+                ", statusType=" + statusType +
+                '}';
+    }
+}
