8454da0e90d63281b25c3ea96a9f726080494f1f
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-01-08 08:22:06 +0530

    create ProjectUserStoryLabelRepository

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/repository/ProjectUserStoryLabelRepository.java
@@ -0,0 +1,10 @@
+package com.swlc.ScrumPepperCPU6001.repository;
+
+import com.swlc.ScrumPepperCPU6001.entity.ProjectUserStoryLabelEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author hp
+ */
+public interface ProjectUserStoryLabelRepository extends JpaRepository<ProjectUserStoryLabelEntity, Long> {
+}
