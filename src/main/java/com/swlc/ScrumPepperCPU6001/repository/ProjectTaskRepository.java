cad0c4afa3a81cd1ec885d221433a87a14bae41d
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-02-02 21:33:21 +0530

    develop project task repository

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/repository/ProjectTaskRepository.java
@@ -0,0 +1,10 @@
+package com.swlc.ScrumPepperCPU6001.repository;
+
+import com.swlc.ScrumPepperCPU6001.entity.ProjectTaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author hp
+ */
+public interface ProjectTaskRepository extends JpaRepository<ProjectTaskEntity, Long> {
+}
