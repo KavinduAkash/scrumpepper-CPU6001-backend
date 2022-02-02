a238df1f31cede498ff4ed4a1e5cc604cea40a14
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-02-02 21:45:00 +0530

    develop AddProjectTaskRequestDTO

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/dto/request/AddProjectTaskRequestDTO.java
@@ -0,0 +1,38 @@
+package com.swlc.ScrumPepperCPU6001.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.swlc.ScrumPepperCPU6001.entity.CorporateEmployeeEntity;
+import com.swlc.ScrumPepperCPU6001.entity.ProjectEntity;
+import com.swlc.ScrumPepperCPU6001.enums.UserStoryStatusType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author hp
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddProjectTaskRequestDTO {
+    private long projectId;
+    private String title;
+    private UserStoryStatusType statusType;
+    private List<Long> employees;
+
+    @Override
+    public String toString() {
+        return "AddProjectTaskRequestDTO{" +
+                "projectId=" + projectId +
+                ", title='" + title + '\'' +
+                ", statusType=" + statusType +
+                ", employees=" + employees +
+                '}';
+    }
+}
