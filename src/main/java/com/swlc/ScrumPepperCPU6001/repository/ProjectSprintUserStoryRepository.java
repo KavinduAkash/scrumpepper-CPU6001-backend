e6cb45fe8199d1aacec4eccd1723b60b2acfd96c
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-02-12 12:59:55 +0530

    develop ProjectSprintUserStoryRepository

---
+++ src/main/java/com/swlc/ScrumPepperCPU6001/repository/ProjectSprintUserStoryRepository.java
@@ -0,0 +1,10 @@
+package com.swlc.ScrumPepperCPU6001.repository;
+
+import com.swlc.ScrumPepperCPU6001.entity.ProjectSprintUserStoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author hp
+ */
+public interface ProjectSprintUserStoryRepository extends JpaRepository<ProjectSprintUserStoryEntity, Long> {
+}
